aece3ddc23d2cff5c030d0cec9fdf059a442fad0
mochoofernandez <143915544+mochoofernandez@users.noreply.github.com>
2023-10-08 16:50:37 -0300

    Add files via upload

---
+++ saludo.js
@@ -0,0 +1,17 @@
+var nombre = "Mauricio";
+var edad = "19";
+var presenteONo = true;
+var apellido = "Licciardi";
+var altura = "1.80";
+
+console.log("Nombre: " + nombre);
+console.log("Nombre: "+ nombre+ ""+typeof nombre);
+console.log("Edad: " + edad);
+console.log("Nombre: "+ edad+ ""+typeof edad);
+console.log("presente: "+ presenteONo);
+console.log("Nombre: "+ presenteONo+ ""+typeof presenteONo);
+console.log("apellido: "+ apellido);
+console.log("Nombre: "+ apellido+ ""+typeof apellido);
+console.log("altura: "+ altura);
+console.log("Nombre: "+ altura+ ""+typeof altura);
+alert("Mi primer hola mundo en javascript" + nombre);
